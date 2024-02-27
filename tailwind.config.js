ba1aed0e0fa212f60fd5f9a8af103ffb5fdf5227
Nick Latcham <nick.latcham@proton.me>
2024-02-26 20:41:58 -0600

    feat: dark mode setting
    needs to be impl'd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    darkMode: ['selector', '[data-mode="dark"]'],
     content: ["./cmd/web/templates/**/*.{html,js}"],
     theme: {
         extend: {},
