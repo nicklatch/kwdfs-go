3caa6d7f19c284952ae7de2abdbac62afcbe4252
Nick <nick.latcham@proton.me>
2024-03-01 12:46:54 -0600

    refactor: change to use db instead of static json

--- internal/server/dealers.go
+++ internal/server/dealers.go
@@ -3,20 +3,33 @@ package server
 import (
 	"log"
 	"net/http"
-	"nicklatch/kwdfs-go/internal/db"
+	"nicklatch/kwdfs-go/internal/database"
+	"time"
 )
+
+var templCache = make(map[time.Time][]database.Dealer)
 
 func (s *Server) DealerHandler(rw http.ResponseWriter, r *http.Request) {
 	data := PageData{Title: "Dealers", Endpoint: "dealers"}
 	err := s.tmpl.ExecuteTemplate(rw, "index.html", data)
 	if err != nil {
-		log.Print(err)
+		log.Println(err)
+		rw.WriteHeader(500)
 	}
+	log.Println(r)
 }
 
 func (s *Server) DealersGetTable(rw http.ResponseWriter, r *http.Request) {
-	err := s.tmpl.ExecuteTemplate(rw, "dealers-table", db.GetAllDealerGroups())
+	data, err := s.db.GetAllDealerGroups()
 	if err != nil {
-		log.Print(err)
+		log.Println(err)
+		rw.WriteHeader(400) // TODO: tmpl fragment to return
 	}
+
+	err = s.tmpl.ExecuteTemplate(rw, "dealers-table", data) // TODO: need to add db service back in here
+	if err != nil {
+		log.Println(err)
+		rw.WriteHeader(500) // TODO: tmpl fragment to return
+	}
+	log.Printf("DealersGetTable: Method: %v", r.Method)
 }
