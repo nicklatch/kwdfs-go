d072b7ebf3a77ead4e6ebec956c473a9a2d75de2
Nick <nick.latcham@proton.me>
2024-03-05 14:18:12 -0600

    build: add gohtml to content extensions array

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
     darkMode: ['selector', '[data-mode="dark"]'],
-    content: ["./cmd/web/templates/**/*.{html,js}"],
+    content: ["./cmd/web/templates/**/*.{html,gohtml,js}"],
     theme: {
         fontFamily: {
             'sans': ['"Noto Sans"', ...defaultTheme.fontFamily.sans]
